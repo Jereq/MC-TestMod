1afc02812dbbd684775c83b5ef4ba368382a3f1c
Sebastian Larsson <sjereq@gmail.com>
2015-09-16 01:49:31 +0200

    Add recipes for rifle and ammo

--- src/main/java/se/jereq/testmod/init/ModRecipes.java
+++ src/main/java/se/jereq/testmod/init/ModRecipes.java
@@ -1,5 +1,6 @@
 package se.jereq.testmod.init;
 
+import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
@@ -13,5 +14,19 @@ public class ModRecipes {
 				'A', ModItems.testItem);
 
 		GameRegistry.addSmelting(ModBlocks.testBlock, new ItemStack(ModItems.testItem, 9), 1000.f);
+
+		GameRegistry.addRecipe(new ItemStack(ModItems.blasterAmmo, 12),
+				" I ",
+				"IRI",
+				"IGI",
+				'I', Items.iron_ingot,
+				'R', Items.redstone,
+				'G', Items.gunpowder);
+
+		GameRegistry.addRecipe(new ItemStack(ModItems.blasterRifle),
+				"IIF",
+				"  I",
+				'I', Items.iron_ingot,
+				'F', Items.flint_and_steel);
 	}
 }
