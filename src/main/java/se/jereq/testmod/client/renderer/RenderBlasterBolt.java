a6b962e17a9587bb024a6358aaa7d20ed8d14e1e
Sebastian Larsson <sjereq@gmail.com>
2015-09-16 18:05:30 +0200

    Change the look of the blaster bolt

--- src/main/java/se/jereq/testmod/client/renderer/RenderBlasterBolt.java
+++ src/main/java/se/jereq/testmod/client/renderer/RenderBlasterBolt.java
@@ -5,8 +5,10 @@ import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.WorldRenderer;
 import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.entity.DataWatcher;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.ResourceLocation;
+import org.lwjgl.opengl.GL11;
 import se.jereq.testmod.entity.EntityBlasterBolt;
 
 import java.util.Random;
@@ -17,99 +19,135 @@ public class RenderBlasterBolt extends Render {
 		super(renderManager);
 	}
 
-	public void doRender(EntityBlasterBolt entity, double x, double y, double z, float p_76986_8_, float partialTicks) {
+	public void doRender(EntityBlasterBolt entity, double x, double y, double z, float rotation, float partialTicks) {
 		Tessellator tessellator = Tessellator.getInstance();
 		WorldRenderer worldrenderer = tessellator.getWorldRenderer();
 		GlStateManager.disableTexture2D();
 		GlStateManager.disableLighting();
 		GlStateManager.enableBlend();
 		GlStateManager.blendFunc(770, 1);
-		double[] xOffsets = new double[8];
-		double[] zOffsets = new double[8];
-		double totX = 0.0D;
-		double totZ = 0.0D;
-		Random random = new Random(entity.boltVertex);
+
+		DataWatcher dw = entity.getDataWatcher();
+		double startX = dw.getWatchableObjectFloat(16);
+		double startY = dw.getWatchableObjectFloat(17);
+		double startZ = dw.getWatchableObjectFloat(18);
 
 		float[] color = {1.f, 0.f, 0.f};
 
-		for (int i = 7; i >= 0; --i)
-		{
-			xOffsets[i] = totX;
-			zOffsets[i] = totZ;
-			totX += (double)(random.nextInt(11) - 5);
-			totZ += (double)(random.nextInt(11) - 5);
-		}
+		double origX = x - (entity.lastTickPosX + (entity.posX - entity.lastTickPosX) * (double)partialTicks);
+		double origY = y - (entity.lastTickPosY + (entity.posY - entity.lastTickPosY) * (double)partialTicks);
+		double origZ = z - (entity.lastTickPosZ + (entity.posZ - entity.lastTickPosZ) * (double)partialTicks);
 
-		for (int k1 = 0; k1 < 4; ++k1)
-		{
-			Random random1 = new Random(entity.boltVertex);
-
-			for (int j = 0; j < 3; ++j)
-			{
-				int k = 7 - j;
-				int l = j == 0 ? 0 : k - 2;
-
-				double prevOffsetX = xOffsets[k] - totX;
-				double prevOffsetZ = zOffsets[k] - totZ;
-
-				for (int height = k; height >= l; --height)
-				{
-					double upperOffsetX = prevOffsetX;
-					double upperOffsetZ = prevOffsetZ;
-
-					double upperWidth = 0.1D + (double)k1 * 0.2D;
-					double lowerWidth = 0.1D + (double)k1 * 0.2D;
-
-					if (j == 0)
-					{
-						prevOffsetX += (double)(random1.nextInt(11) - 5);
-						prevOffsetZ += (double)(random1.nextInt(11) - 5);
-
-						upperWidth *= (double)height * 0.1D + 1.0D;
-						lowerWidth *= (double)(height - 1) * 0.1D + 1.0D;
-					}
-					else
-					{
-						prevOffsetX += (double)(random1.nextInt(31) - 15);
-						prevOffsetZ += (double)(random1.nextInt(31) - 15);
-					}
-
-					final int GL_TRIANGLE_STRIP = 5;
-					worldrenderer.startDrawing(GL_TRIANGLE_STRIP);
-					worldrenderer.setColorRGBA_F(color[0], color[1], color[2], 0.3F);
-
-					for (int edgeNum = 0; edgeNum < 5; ++edgeNum)
-					{
-						double upperBaseX = x - upperWidth;
-						double upperBaseZ = z - upperWidth;
-
-						double lowerBaseX = x - lowerWidth;
-						double lowerBaseZ = z - lowerWidth;
-
-						if (edgeNum == 1 || edgeNum == 2)
-						{
-							upperBaseX += upperWidth * 2.0D;
-							lowerBaseX += lowerWidth * 2.0D;
-						}
-
-						if (edgeNum == 2 || edgeNum == 3)
-						{
-							upperBaseZ += upperWidth * 2.0D;
-							lowerBaseZ += lowerWidth * 2.0D;
-						}
-
-						worldrenderer.addVertex(lowerBaseX + prevOffsetX, y + (double)(height * 16), lowerBaseZ + prevOffsetZ);
-						worldrenderer.addVertex(upperBaseX + upperOffsetX, y + (double)((height + 1) * 16), upperBaseZ + upperOffsetZ);
-					}
-
-					tessellator.draw();
-				}
-			}
+		for (int layer = 0; layer < 4; ++layer) {
+			generateBar(tessellator, origX + startX, origY + startY, origZ + startZ, x, y, z,
+					layer * 0.01 + 0.01, color[0], color[1], color[2], 0.3F);
 		}
 
 		GlStateManager.disableBlend();
 		GlStateManager.enableLighting();
 		GlStateManager.enableTexture2D();
+	}
+
+	private void generateBar(Tessellator tessellator, double startX, double startY, double startZ, double endX, double endY, double endZ, double size, float r, float g, float b, float a) {
+		double deltaX = endX - startX;
+		double deltaY = endY - startY;
+		double deltaZ = endZ - startZ;
+
+		double magX = Math.abs(deltaX);
+		double magY = Math.abs(deltaY);
+		double magZ = Math.abs(deltaZ);
+
+		final double small = 0.001d;
+		if (magX < small && magY < small && magZ < small) {
+			return;
+		}
+
+		double side1X;
+		double side1Y;
+		double side1Z;
+
+		double side2X;
+		double side2Y;
+		double side2Z;
+
+		if (magX < magY && magX < magZ) {
+			side1X = 0;
+			side1Y = -deltaZ;
+			side1Z = deltaY;
+
+			side2X = deltaY * deltaY + deltaZ * deltaZ;
+			side2Y = -deltaX * deltaY;
+			side2Z = -deltaX * deltaZ;
+		} else if (magY < magZ) {
+			side1X = deltaZ;
+			side1Y = 0;
+			side1Z = -deltaX;
+
+			side2X = -deltaY * deltaX;
+			side2Y = deltaZ * deltaZ + deltaX * deltaX;
+			side2Z = -deltaY * deltaZ;
+		} else {
+			side1X = -deltaY;
+			side1Y = deltaX;
+			side1Z = 0;
+
+			side2X = -deltaZ * deltaX;
+			side2Y = -deltaZ * deltaY;
+			side2Z = deltaX * deltaX + deltaY * deltaY;
+		}
+
+		double side1Length = Math.sqrt(side1X * side1X + side1Y * side1Y + side1Z * side1Z);
+		side1X /= side1Length;
+		side1Y /= side1Length;
+		side1Z /= side1Length;
+
+		double side2Length = Math.sqrt(side2X * side2X + side2Y * side2Y + side2Z * side2Z);
+		side2X /= side2Length;
+		side2Y /= side2Length;
+		side2Z /= side2Length;
+
+		double start1X = startX + side1X * size;
+		double start2X = startX + side2X * size;
+		double start3X = startX - side1X * size;
+		double start4X = startX - side2X * size;
+		double start1Y = startY + side1Y * size;
+		double start2Y = startY + side2Y * size;
+		double start3Y = startY - side1Y * size;
+		double start4Y = startY - side2Y * size;
+		double start1Z = startZ + side1Z * size;
+		double start2Z = startZ + side2Z * size;
+		double start3Z = startZ - side1Z * size;
+		double start4Z = startZ - side2Z * size;
+
+		double end1X = endX + side1X * size;
+		double end2X = endX + side2X * size;
+		double end3X = endX - side1X * size;
+		double end4X = endX - side2X * size;
+		double end1Y = endY + side1Y * size;
+		double end2Y = endY + side2Y * size;
+		double end3Y = endY - side1Y * size;
+		double end4Y = endY - side2Y * size;
+		double end1Z = endZ + side1Z * size;
+		double end2Z = endZ + side2Z * size;
+		double end3Z = endZ - side1Z * size;
+		double end4Z = endZ - side2Z * size;
+
+		WorldRenderer renderer = tessellator.getWorldRenderer();
+		renderer.startDrawing(GL11.GL_TRIANGLE_STRIP);
+		renderer.setColorRGBA_F(r, g, b, a);
+
+		renderer.addVertex(end1X, end1Y, end1Z);
+		renderer.addVertex(start1X, start1Y, start1Z);
+		renderer.addVertex(end2X, end2Y, end2Z);
+		renderer.addVertex(start2X, start2Y, start2Z);
+		renderer.addVertex(end3X, end3Y, end3Z);
+		renderer.addVertex(start3X, start3Y, start3Z);
+		renderer.addVertex(end4X, end4Y, end4Z);
+		renderer.addVertex(start4X, start4Y, start4Z);
+		renderer.addVertex(end1X, end1Y, end1Z);
+		renderer.addVertex(start1X, start1Y, start1Z);
+
+		tessellator.draw();
 	}
 
 	@Override
@@ -118,7 +156,7 @@ public class RenderBlasterBolt extends Render {
 	}
 
 	@Override
-	public void doRender(Entity entity, double x, double y, double z, float p_76986_8_, float partialTicks) {
-		this.doRender((EntityBlasterBolt) entity, x, y, z, p_76986_8_, partialTicks);
+	public void doRender(Entity entity, double x, double y, double z, float rotation, float partialTicks) {
+		this.doRender((EntityBlasterBolt) entity, x, y, z, rotation, partialTicks);
 	}
 }
--- src/main/java/se/jereq/testmod/entity/EntityBlasterBolt.java
+++ src/main/java/se/jereq/testmod/entity/EntityBlasterBolt.java
@@ -1,9 +1,13 @@
 package se.jereq.testmod.entity;
 
+import net.minecraft.block.Block;
+import net.minecraft.entity.DataWatcher;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.projectile.EntityThrowable;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.MovingObjectPosition;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
 
 public class EntityBlasterBolt extends EntityThrowable {
@@ -11,20 +15,56 @@ public class EntityBlasterBolt extends EntityThrowable {
 	public static final String name = "testmod_blasterBolt";
 
 	public long boltVertex;
+	public double startX;
+	public double startY;
+	public double startZ;
 
 	public EntityBlasterBolt(World worldIn) {
 		super(worldIn);
 		boltVertex = this.rand.nextLong();
+		startX = posX;
+		startY = posY;
+		startZ = posZ;
+
+		DataWatcher dw = getDataWatcher();
+		dw.updateObject(16, (float)startX);
+		dw.updateObject(17, (float)startY);
+		dw.updateObject(18, (float) startZ);
 	}
 
 	public EntityBlasterBolt(World worldIn, EntityLivingBase throwerIn) {
 		super(worldIn, throwerIn);
 		boltVertex = this.rand.nextLong();
+		startX = posX;
+		startY = posY;
+		startZ = posZ;
+
+		DataWatcher dw = getDataWatcher();
+		dw.updateObject(16, (float)startX);
+		dw.updateObject(17, (float)startY);
+		dw.updateObject(18, (float)startZ);
 	}
 
 	public EntityBlasterBolt(World worldIn, double x, double y, double p_i1778_6_) {
 		super(worldIn, x, y, p_i1778_6_);
 		boltVertex = this.rand.nextLong();
+		startX = posX;
+		startY = posY;
+		startZ = posZ;
+
+		DataWatcher dw = getDataWatcher();
+		dw.updateObject(16, (float)startX);
+		dw.updateObject(17, (float)startY);
+		dw.updateObject(18, (float)startZ);
+	}
+
+	@Override
+	protected void entityInit() {
+		super.entityInit();
+		DataWatcher dw = getDataWatcher();
+		dw.addObject(16, 0.f);
+		dw.addObject(17, 0.f);
+		dw.addObject(18, 0.f);
 	}
 
 	@Override
@@ -49,6 +89,35 @@ public class EntityBlasterBolt extends EntityThrowable {
 
 	@Override
 	protected float getVelocity() {
-		return 5.f;
+		return 6.f;
+	}
+
+	/**
+	 * (abstract) Protected helper method to write subclass entity data to NBT.
+	 */
+	@Override
+	public void writeEntityToNBT(NBTTagCompound tagCompound)
+	{
+		super.writeEntityToNBT(tagCompound);
+		tagCompound.setDouble("startX", startX);
+		tagCompound.setDouble("startY", startY);
+		tagCompound.setDouble("startZ", startZ);
+	}
+
+	/**
+	 * (abstract) Protected helper method to read subclass entity data from NBT.
+	 */
+	@Override
+	public void readEntityFromNBT(NBTTagCompound tagCompound)
+	{
+		super.readEntityFromNBT(tagCompound);
+		startX = tagCompound.getDouble("startX");
+		startY = tagCompound.getDouble("startY");
+		startZ = tagCompound.getDouble("startZ");
+
+		DataWatcher dw = getDataWatcher();
+		dw.updateObject(16, (float)startX);
+		dw.updateObject(17, (float)startY);
+		dw.updateObject(18, (float)startZ);
 	}
 }
